b08c02692db17e887ff94b62f85d8ac489b96770
Nicole Valentini <ncvalentini@gmail.com>
2022-03-24 17:39:02 -0600

    Add css files for components

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = (props) => {
+  return (
+    <div className='movie-card'>
+      <h3>Movie Title</h3>
+      <img>src= </img>
+    </div>
+  )
+}
+
+
+
+export default Card;
