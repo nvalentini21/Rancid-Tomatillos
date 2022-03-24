db77a99680a9b34df770910e8ff5a868f2bf42f2
Nicole Valentini <ncvalentini@gmail.com>
2022-03-24 17:15:20 -0600

    set state to movie data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import Movies from './Movies';
 import Nav from './Nav';
+import movieData from './data';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: []
+      movies: movieData.movies
     }
   }
 
   render() {
+    console.log(this.state.movies)
     return (
       <main className='App'>
         <nav>RANCID TOMATILLOS NAV</nav> //component Nav will be here
