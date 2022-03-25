bd4483982cb59f10e0e778fd9ea4d3f9330f197d
Nicole Valentini <ncvalentini@gmail.com>
2022-03-24 19:12:14 -0600

    Display movies in movie grid, style background

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Card.css'
 
-const Card = (props) => {
+const Card = ({poster, backdrop, rating, release_date, title}) => {
   return (
     <div className='movie-card'>
-      <h3>Movie Title</h3>
-      <img>src= </img>
+      <h3>{title}</h3>
+      <img src={poster} alt="Movie poster" width="200"/>
     </div>
   )
 }
--- src/Movies.js
+++ src/Movies.js
@@ -2,10 +2,22 @@ import React from 'react';
 import Card from './Card';
 import './Movies.css';
 
-const Movies = () => {
-
+const Movies = ({movies}) => {
+  const movieCards = movies.map(movie => {
+    return (
+      <Card
+        id={movie.id}
+        key={movie.id}
+        poster={movie.poster_path}
+        backdrop={movie.backdrop_path}
+        rating={movie.average_rating}
+        release_date={movie.release_date}
+        title={movie.title}
+      />
+    )
+  })
   return (
-    <div className='movies-container'>Movie Cards will show here</div>
+    <div className='movies-container'>{movieCards}</div>
   )
 }
 
