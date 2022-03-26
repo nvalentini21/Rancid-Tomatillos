2e34766bcfaf87066b61827caef81bad7e1093ce
Nicole Valentini <ncvalentini@gmail.com>
2022-03-26 13:41:31 -0600

    Fix app width to fit content

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,10 +1,10 @@
 
 
-
 const fetchCalls = {
   fetchData: function (endpoint) {
     return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${endpoint}`)
       .then(response => response.json())
+      .catch(error => window.alert('Ooops! The server is down.'))
   }
 }
 
