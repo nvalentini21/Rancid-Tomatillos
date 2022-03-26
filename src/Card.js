009cefa6aeb26498a51b09d10aea80228b451bc7
Nicole Valentini <ncvalentini@gmail.com>
2022-03-26 16:33:00 -0600

    Add single movie info to page and re-style

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import './Card.css'
 
 const Card = ({poster, backdrop, rating, release_date, title, id, setSingleMovie}) => {
   return (
-    <div className='movie-card' onClick={() => setSingleMovie(id)}>
+    <div className='movie-card' cursor="pointer" onClick={() => setSingleMovie(id)}>
       <img src={poster} alt="Movie poster" width="200"/>
     </div>
   )
--- src/Movie.js
+++ src/Movie.js
@@ -2,16 +2,25 @@ import React from 'react';
 import './Movie.css'
 
 const Movie= ({movie}) => {
+
+const releaseDateObj = new Date(movie.release_date)
+const releaseDate = (releaseDateObj.toString().split(' ').slice(1, 4).join(' '))
+const genres = movie.genres.join(', ')
+const rating = movie.average_rating.toFixed(2)
+
   return (
     <div className='single-movie'>
       <div className='poster-div'>
         <h1>{movie.title}</h1>
-        <p>{movie.release_date}</p>
-        <img src={movie.poster_path} alt="Movie poster" width="350" height="500"/>
+        <p>Runtime: {movie.runtime} minutes </p>
+        <p>Date Released: {releaseDate}</p>
+        <img src={movie.poster_path} alt="Movie poster" width="95%" height="auto"/>
       </div>
       <div className='description-div'>
-        <h3>Genre List Here</h3>
-        <p>Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!</p>
+        <h3>Genres: {genres}</h3>
+        <h4>Overview:</h4>
+        <p>{movie.overview}</p>
+        <p>Average Rating: {rating}</p>
       </div>
     </div>
   )
